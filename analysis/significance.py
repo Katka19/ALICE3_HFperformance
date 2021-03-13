93c6232d3e93b5130cff0ef56f673ba74cb36265
Gian Michele Innocenti <gminnocen@gmail.com>
2021-03-14 00:56:00 +0100

    Style improvement (#13)

--- analysis/significance.py
+++ analysis/significance.py
@@ -29,15 +29,20 @@ def analysis(hadron="Lambda_c", collision="pp14p0", yrange="absy3p0", \
     with open(r'databases/theory_yields.yaml') as fileyields:
         paramyields = yaml.load(fileyields, Loader=yaml.FullLoader)
 
+    textcollision = paramgen["text_string"][collision]
+    textrapid = paramgen["text_string"][yrange]
+    textmodel = paramgen["text_string"][model]
+
     sigma_aa_b = paramgen["statistics"][collision]["sigmaAA_b"]
     lumiaa_monthi_invnb = paramgen["statistics"][collision]["lumiAA_monthi_invnb"]
     nevt = sigma_aa_b * lumiaa_monthi_invnb * 1e9
+    #nevt = 2.*1e9
     bratio = paramgen["branchingratio"][hadron][brmode]
 
     yieldmid = paramyields[model][collision][yrange][hadron]
-    #text = '%s N_{ev}(%s) = %.1f B, BR=%.5f%%' \
-    #        % (model, collision, nevt/1e9, bratio*100)
-    #print (text)
+    text = '%s, N_{ev} = %.0f 10^{12}' % (textmodel, nevt/1e12)
+    text_a = '#Lambda_{c} #rightarrow pK#pi, %s, BR=%.2f%%' % (textrapid, bratio*100)
+    text_b = 'ALICE3 projection, with IRIS, no PID, %s' % textcollision
     fileeff = TFile(nfileeff)
     histoeff = fileeff.Get(nhistoeff)
     filebkg = TFile(nfilebkg)
@@ -48,6 +53,9 @@ def analysis(hadron="Lambda_c", collision="pp14p0", yrange="absy3p0", \
 
     if use_unnorm == 1:
         histodndptth = fileyieldth.Get(nhistoyieldth)
+        histodndptth.Scale(1./70000.) #TEMPORARY this is a fix to account for the
+                                      #conversion from a cross-section in mub
+                                      #to yields, sigma=70000 mub
     else:
         histodndptth = fileyieldth.Get(nhistoyieldth_norm)
         histodndptth.Scale(yieldmid)
@@ -77,8 +85,8 @@ def analysis(hadron="Lambda_c", collision="pp14p0", yrange="absy3p0", \
     canvas.cd()
     gPad.SetLogy()
 
-    hempty = TH2F("hempty", ";p_{T}; Significance(3#sigma)", 100, 0., 12., 100, ymin, ymax)
-    hempty.GetXaxis().SetTitle("p_{T}")
+    hempty = TH2F("hempty", ";p_{T} (GeV/c); Significance(3#sigma)", 100, 0., 12., 100, ymin, ymax)
+    hempty.GetXaxis().SetTitle("p_{T} (GeV/c)")
     hempty.GetXaxis().SetLabelFont(42)
     hempty.GetXaxis().SetTitleOffset(1)
     hempty.GetXaxis().SetTitleFont(42)
@@ -105,8 +113,32 @@ def analysis(hadron="Lambda_c", collision="pp14p0", yrange="absy3p0", \
 
     histosignfperevent.SetLineColor(1)
     histosignfperevent.SetMarkerColor(1)
-    histosignfperevent.SetLineWidth(2)
+    histosignfperevent.SetLineWidth(1)
+    histosignf.SetLineColor(1)
+    histosignf.SetMarkerColor(1)
+    histosignf.SetLineWidth(2)
     histosignf.Draw("same")
+    t_b = TLatex()
+    t_b.SetNDC()
+    t_b.SetTextFont(42)
+    t_b.SetTextColor(1)
+    t_b.SetTextSize(0.035)
+    t_b.SetTextAlign(12)
+    t_b.DrawLatex(0.2, 0.85, text_b)
+    t_c = TLatex()
+    t_c.SetNDC()
+    t_c.SetTextFont(42)
+    t_c.SetTextColor(1)
+    t_c.SetTextSize(0.03)
+    t_c.SetTextAlign(12)
+    t_c.DrawLatex(0.2, 0.80, text)
+    t_a = TLatex()
+    t_a.SetNDC()
+    t_a.SetTextFont(42)
+    t_a.SetTextColor(1)
+    t_a.SetTextSize(0.035)
+    t_a.SetTextAlign(12)
+    t_a.DrawLatex(0.2, 0.75, text_a)
     canvas.SaveAs(hadron+"_results.pdf")
     canvas.SaveAs(hadron+"_results.C")
 
